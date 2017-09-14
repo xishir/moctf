3e369684eada47751b2825a90f9910ae87848d1e
xishir <daoyuan615958739@qq.com>
2017-09-14 19:34:19 +0800

    add web WriteUp

--- WEB/web10-火眼金睛/work.php
+++ WEB/web10-火眼金睛/work.php
@@ -3,7 +3,7 @@
 	@$rand_demo = $_SESSION['rand'];
 	@$answer = md5(md5((string)$_POST['answer']));
 	if ($answer == $rand_demo) {
-		echo "<script language='javascript'>alert('moctf{Programming_1s_important_!!}');window.location='index.php'</script>";
+		echo "<script language='javascript'>alert('moctf{xxxxxxxxxx}');window.location='index.php'</script>";
 	}else{
 		echo "<script language='javascript'>alert('Error!!!');window.location='index.php'</script>";
 	}
